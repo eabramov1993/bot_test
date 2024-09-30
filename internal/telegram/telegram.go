3407f269bdbdb4d72867586d0d977c140eff6170
yabramov <abramov@MacBook-Pro-Evgenij.local>
2024-09-30 13:51:31 +0500

    Deploy to Heroku

--- internal/telegram/telegram.go
+++ internal/telegram/telegram.go
@@ -2,12 +2,13 @@ package telegram
 
 import (
 	"log"
+	"os"
 	"test_bot/internal/handlers"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-var botToken = "7527167132:AAENsRMPyPa2k0X4wrE0-fjVZyUOAE6GKNE"
+var botToken = os.Getenv("TELEGRAM_TOKEN")
 
 // StartBot запускает бота
 func StartBot() error {
