be48953e6aee350a490e47aa7889c3b32de35e50
yabramov <abramov@MacBook-Pro-Evgenij.local>
2024-09-30 13:59:17 +0500

    Run bot and HTTP server for Heroku

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"os"
 	"test_bot/internal/telegram"
 )
 
 func main() {
-	err := telegram.StartBot()
+	// Запускаем бота в отдельной горутине
+	go func() {
+		err := telegram.StartBot()
+		if err != nil {
+			log.Fatalf("Ошибка запуска бота: %v", err)
+		}
+	}()
+
+	// Получаем порт из переменной окружения
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable not set")
+	}
+
+	// Запускаем HTTP-сервер для Heroku
+	log.Printf("Server is running on port %s", port)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
-		log.Fatalf("Ошибка запуска бота: %v", err)
+		log.Fatalf("Ошибка запуска HTTP сервера: %v", err)
 	}
 }
